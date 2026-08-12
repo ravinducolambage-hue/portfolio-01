20ea113cc5a694a958d4912d54be72dfc973f71f
Your Name <ravinducolambage@gmail.com>
2026-08-12 11:30:40 +0530

    Fix undefined REFERENCES_DATA variable in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2178,7 +2178,7 @@ function ReferencesSection({
         </p>
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
-          {REFERENCES_DATA.map((ref, i) => (
+          {references.map((ref, i) => (
             <motion.div
               key={ref.id}
               initial={{ opacity: 0, y: 30 }}
